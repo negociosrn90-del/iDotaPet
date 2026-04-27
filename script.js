86184bc8b4a8007e492d8f3a4e2cbcdcd77a1e12
Ruan Rodrigues <negociosrn90@gmail.com>
2026-04-26 23:05:18 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const animais = [
   { nome: "Rex", tipo: "cachorro", idade: "2 anos", descricao: "Brincalhão", imagem: "https://placedog.net/300/200?id=1" },
   { nome: "Luna", tipo: "cachorro", idade: "1 ano", descricao: "Carinhosa", imagem: "https://placedog.net/300/200?id=2" },
   { nome: "Thor", tipo: "cachorro", idade: "3 anos", descricao: "Protetor", imagem: "https://placedog.net/300/200?id=3" },
-  { nome: "Mia", tipo: "gato", idade: "2 anos", descricao: "Tranquila", imagem: "https://placekitten.com/300/200" },
-  { nome: "Simba", tipo: "gato", idade: "1 ano", descricao: "Brincalhão", imagem: "https://placekitten.com/301/200" },
-  { nome: "Nina", tipo: "gato", idade: "4 anos", descricao: "Muito dócil", imagem: "https://placekitten.com/302/200" }
+  { nome: "Mia", tipo: "gato", idade: "2 anos", descricao: "Tranquila", imagem: "https://cataas.com/cat?width=300&height=200" },
+  { nome: "Simba", tipo: "gato", idade: "1 ano", descricao: "Brincalhão", imagem: "https://cataas.com/cat?width=301&height=200" },
+  { nome: "Nina", tipo: "gato", idade: "4 anos", descricao: "Muito dócil", imagem: "https://cataas.com/cat?width=302&height=200" }
 ];
 
 const lista = document.getElementById("lista-animais");
